bb1930d9460cd63d741792ed88a309e83f541734
Piotr Stus <pm2024love@gmail.com>
2024-06-22 11:48:00 +0200

    Controler ueser

---
+++ PageMonitor.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PageMonitor.WebApi.Controllers
+{
+    // jest to klasa abstrakcyjna, która dziediczy po ControllerBase
+    // dzieki czemu nasze kontrolery nie będą już musiały po tej klasie dziediczyć
+    public abstract class BaseController : ControllerBase
+    {
+        // w kazdym kontrolerze bedziemy miec loger
+        protected readonly ILogger _logger;
+        // i mediatora, dlatego ze za pomoca mediatora bedziemy wysylac nasze
+        // query i commandy
+        // mediatora mozna zamienic na interfejs ISender to jest interfejs z mediatora
+        // ktory jest ograniczony do metody send
+        // mediator zawiera jeszcze metode publish, ktora dodatkowo ma wysylac
+        // rownolegle do roznych odbiorcow wiadomosci
+        // tutaj z przyzwyczajenie uzywamy mediatora
+        protected readonly IMediator _mediator;
+
+        public BaseController(ILogger logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+    }
+}
---
+++ PageMonitor.WebApi/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PageMonitor.Application.Logic.User;
+
+namespace PageMonitor.WebApi.Controllers
+{
+
+    // zmienamy routing w tej chwili dzialat tak, ze mamy adres api
+    // a nastepnie adres kontrollera
+    //[Route("api/[controller]")]
+    // natomiast my jeszcze chcemy adres akcji dlatego tez
+    // wiec teraz nie ma prefiksu api
+    // nestepnie jest nazwa kontrolera i akcji ktora wywolujemy
+    [Route("[controller]/[action]")]
+    [ApiController]
+    //dzieiczy po BaseControllerze
+    public class UserController : BaseController
+    {
+        public UserController(ILogger logger, IMediator mediator) : base(logger, mediator)
+        {
+        }
+
+        // dodajemy akcje, ktora wywola naszego commanda
+        // akcja Create typu POST tworzącą nowe konto w aplikacji
+        // dobrze, zeby nazwa odpowiadala nazwa commanda
+        // w parametrze jest request, czyli nasz request z commanda
+        // zawartosc dane do tej klasy sa odczytywane z body requesta
+        // jest to requesta typu post
+        [HttpPost]
+        public async Task<ActionResult> CreateUserWithAccount([FromBody] CreateUserWithAccountCommand.Request model)
+        {
+            // to co robimy to bierzemy mediatora i wysylamy obiekt o nazwie model (nasz request)
+            // mediator zajmie sie wyslaniem tego i przekazaniem do handlera
+            // ktory znajduje sie w klasie CreateUserWithAccountCommand
+            // w projekcie Application
+            var createAccountResult = await _mediator.Send(model);
+            return Ok(createAccountResult);
+        }
+    }
+}
