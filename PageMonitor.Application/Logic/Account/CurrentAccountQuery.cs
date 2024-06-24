717f160f53445cc792be8cabfd9a3b9e968132db
Piotr Stus <pm2024love@gmail.com>
2024-06-24 17:06:06 +0200

    fetching account data

---
+++ PageMonitor.Application/Logic/Account/CurrentAccountQuery.cs
@@ -0,0 +1,64 @@
+﻿using EFCoreSecondLevelCacheInterceptor;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PageMonitor.Application.Exceptions;
+using PageMonitor.Application.Interfaces;
+using PageMonitor.Application.Logic.Abstractions;
+using PageMonitor.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PageMonitor.Application.Logic.User.CreateUserWithAccountCommand.Request;
+
+namespace PageMonitor.Application.Logic.Account
+{
+    public static class CurrentAccountQuery
+    {
+        // parametry requesta beda puste
+        public class Request : IRequest<Result>
+        {
+
+        }
+
+        // natomiast to co bedziemy zwracac to aktualnie name konta
+        // mozna zwracac inne
+        public class Result
+        {
+            public required string Name { get; set; }
+        }
+
+        // zmieniamy klasę bazową z BaseCommandHandler na BaseQuerydHandler
+        public class Handler : BaseCommandHandler, IRequestHandler<Request, Result>
+        {
+            public Handler(ICurrentAccountProvider currentAccountProvider,
+                IApplicationDbContext applicationDbContext
+                ) : base(currentAccountProvider, applicationDbContext)
+            {
+            }
+
+            public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
+            {
+                // GetAuthenticatedAccount - metoda ICurrentAccountProvidera, ktora juz implementowalismy wczesniej
+                // odczytuje aktualne konto z ciastka no i jak nie istnieje to rzuca wyjatki
+                // a jak istnieje to wyciaga sobie z bazy i zwraca cale to konto
+                var account = await _currentAccountProvider.GetAuthenticatedAccount();
+                return new Result()
+                {
+                    Name = account.Name
+                };
+            }
+        }
+
+        public class Validator : AbstractValidator<Request>
+        {
+            public Validator()
+            {
+                // walidator pozostaje pusty bo nie mamy zadnych parametrow requesta
+            }
+        }
+
+    }
+}
---
+++ PageMonitor.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using PageMonitor.Application.Logic.Account;
+using PageMonitor.Application.Logic.User;
+using PageMonitor.Infrastructure.Auth;
+using PageMonitor.WebApi.Application.Auth;
+using PageMonitor.WebApi.Application.Response;
+
+namespace PageMonitor.WebApi.Controllers
+{
+
+    // zmienamy routing w tej chwili dzialat tak, ze mamy adres api
+    // a nastepnie adres kontrollera
+    //[Route("api/[controller]")]
+    // natomiast my jeszcze chcemy adres akcji dlatego tez
+    // wiec teraz nie ma prefiksu api
+    // nestepnie jest nazwa kontrolera i akcji ktora wywolujemy
+    [Route("[controller]/[action]")]
+    [ApiController]
+    //dzieiczy po BaseControllerze
+    public class AccountController : BaseController
+    {
+
+        public AccountController(ILogger<AccountController> logger,
+            IMediator mediator) : base(logger, mediator)
+        {
+        }
+
+        // potrzebujemy w zasadzie tylko jednej metody, czyli pobierania naszego
+        // konta
+        [HttpGet]
+        public async Task<ActionResult> GetCurrentAccount()
+        {
+            var data = await _mediator.Send(new CurrentAccountQuery.Request() { });
+            return Ok(data);
+        }
+    }
+}
