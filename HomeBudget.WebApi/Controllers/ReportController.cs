3c88ecfa48b9c4cc36199186c06752e8746367ba
Piotr Stus <pm2024love@gmail.com>
2024-10-14 13:43:02 +0200

    Added ReportController, GetTransactionsQuery.

---
+++ HomeBudget.WebApi/Controllers/ReportController.cs
@@ -0,0 +1,24 @@
+﻿using HomeBudget.Application.Logic.Budget.Category;
+using HomeBudget.Application.Logic.Budget.Report;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBudget.WebApi.Controllers
+{
+
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ReportController : BaseController
+    {
+        public ReportController(ILogger<TransactionController> logger, IMediator mediator) : base(logger, mediator)
+        {
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetTransactions([FromQuery] GetTransactionsQuery.Request model)
+        {
+            var data = await _mediator.Send(model);
+            return Ok(data);
+        }
+    }
+}
